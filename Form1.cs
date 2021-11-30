c1d0031d707d604faeb91ed8f073a21a7d9e5901
k31371 <k31371@207-20>
2021-11-30 17:14:26 +0500

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IS_2_19_KitaevDV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        abstract class HWComponents
+        {
+            public string cost;
+            public string year;
+            public HWComponents(string cost, string year)
+            {
+                cost = "null";
+                year = "null";
+            }
+            public virtual void Display()
+            {
+                listBox1.Items.Add("");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Menu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IS_2_19_KitaevDV
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 Form1 = new Form1();
+            Form1.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 Form1 = new Form2();
+            Form2.ShowDialog();
+        }
+    }
+}
