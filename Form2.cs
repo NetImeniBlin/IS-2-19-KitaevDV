caa630a17175273b9389b685c8f9206ced78e643
k31371 <k31371@207-20>
2021-11-30 19:05:45 +0500

    aaaaaaaaaa

--- Form2.cs
+++ Form2.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace IS_2_19_KitaevDV
 {
@@ -15,6 +16,29 @@ namespace IS_2_19_KitaevDV
         public Form2()
         {
             InitializeComponent();
+        }
+        MySqlConnection conn;
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            string connstr = "server=caseum.ru;port=33333;user=test_user;database=db_test;password=test_pass;";
+            conn = new MySqlConnection(connstr);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+            }
+            catch
+            {
+                MessageBox.Show("nea");
+            }
+            finally
+            {
+                conn.Close;
+            }
         }
     }
 }
