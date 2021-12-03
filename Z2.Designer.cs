d3eeb5a44189d3e021da539052fe5164bda7974f
Net Imeni <gagatushkan@gmail.com>
2021-12-03 22:48:45 +0500

    дизайн

--- Z2.Designer.cs
+++ Z2.Designer.cs
@@ -34,21 +34,21 @@ namespace IS_2_19_KitaevDV
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(292, 122);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(518, 188);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "проверка";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Form2
+            // Z2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(542, 212);
             this.Controls.Add(this.button1);
-            this.Name = "Form2";
+            this.Name = "Z2";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
