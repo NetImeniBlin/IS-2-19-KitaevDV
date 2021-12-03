4cd602d8be1067976d9d5cf345a33afcd0d05f67
NetImeniBlin <94395843+NetImeniBlin@users.noreply.github.com>
2021-12-03 23:03:03 +0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Podklvkl
+    {
+        public string Vklpodkl = "server=caseum.ru;port=33333;user=test_user;database=db_test;password=test_pass;";
+    }
+}
